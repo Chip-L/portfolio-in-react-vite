690ebd8d86051fe5ef70b283b3cdec6766b370f6
clong desktop <chip_long@yahoo.com>
2024-09-29 10:30:43 -0600

    added vitest and prettier pkgs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
+
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  server: { open: true },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
+    coverage: {
+      provider: "istanbul",
+      reporter: ["text", "json", "html"],
+      thresholds: {
+        lines: 85,
+        branches: 70,
+        functions: 70,
+        statements: 85,
+      },
+      include: ["src/**"],
+      exclude: ["src/main.tsx"],
+      skipFull: true, // don't show if 100% covered
+    },
+  },
+});
